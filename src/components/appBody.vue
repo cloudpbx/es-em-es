192c9c5478a117f9bac2d2f674dfe049bf683d52
Edmund Yao <eyao5@my.bcit.ca>
2019-12-02 12:33:22 -0800

    added recieve and fixed vue layout

--- src/components/appBody.vue
+++ src/components/appBody.vue
@@ -1,19 +1,28 @@
-<template>
-  <v-container>
+<template >
+  <div style="width:100%; height:100%">
     <v-container class="grey lighten-5">
-      <v-form @submit.prevent="test"
-        ><v-text-field v-model="text"></v-text-field>
+      <v-form @submit.prevent="test">
+        <v-text-field v-model="text"></v-text-field>
 
         <v-btn color="gray" type="submit">Test</v-btn>
       </v-form>
     </v-container>
-  </v-container>
+
+    <contactList style="position:absolute; right:250px; top:0;" />
+  </div>
 </template>
 
 <script>
 /* eslint-disable */
 
+import contactList from "./contactList";
+
 export default {
+  name: "appBody",
+  components: {
+    contactList
+  },
+
   data() {
     return {
       text: null
