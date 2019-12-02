ef671192b0739ced789c9c204f536ba31dfb29b2
Edmund Yao <eyao5@my.bcit.ca>
2019-12-02 12:37:54 -0800

    fixed a merge conflict

--- src/components/appBody.vue
+++ src/components/appBody.vue
@@ -1,15 +1,10 @@
 <template >
   <div style="width:100%; height:100%">
     <v-container class="grey lighten-5">
+      <messageWindow :history="selectedPhoneHistory" />
 
       <v-form @submit.prevent="test">
         <v-text-field v-model="text"></v-text-field>
-
-    <messageWindow :history="selectedPhoneHistory" />
-
-      <v-form @submit.prevent="test"
-        ><v-text-field v-model="text"></v-text-field>
-
 
         <v-btn color="gray" type="submit">Test</v-btn>
       </v-form>
@@ -22,14 +17,9 @@
 <script>
 /* eslint-disable */
 
-
-
-
-
-
 import messageWindow from "./messageWindow";
 import contactList from "./contactList";
-import {mockdata} from "./recieve";
+import { mockdata } from "./recieve";
 
 export default {
   components: {
@@ -40,7 +30,7 @@ export default {
   data() {
     return {
       text: null,
-      selectedPhoneHistory: mockdata["+15555555"],
+      selectedPhoneHistory: mockdata["+15555555"]
     };
   },
   methods: {
