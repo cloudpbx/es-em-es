b77c6885bf689d91c66e1189f877384e951db8e8
Mohamed Laradji <malaradji@gmail.com>
2019-12-02 11:33:51 -0800

    feat(webhooks): Use POST instead of GET

--- src/webhook-server/__main__.py
+++ src/webhook-server/__main__.py
@@ -6,14 +6,13 @@ from aiohttp import web
 
 routes = web.RouteTableDef()
 
-@routes.get('/webhook/oV2KDfSKNQb1SRMGsRzJ')
+@routes.post('/webhook/oV2KDfSKNQb1SRMGsRzJ')
 async def incoming_webhook(request: web.Request):
     data = await request.json()
     print(data)
     return web.Response(text="Hello, world")
 
 if __name__ == "__main__":
-    print("hi")
     app = web.Application()
     app.add_routes(routes)
     web.run_app(app)
