2188c3188f72b3d3e6db8c6367ca35bd28348c53
Edmund Yao <eyao5@my.bcit.ca>
2019-12-01 19:23:00 -0800

    fixed some eslint stuff

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -40,12 +40,7 @@
 export default {
   data () {
     return {
-      items: [
-        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-        { title: 'Photos', icon: 'mdi-image' },
-        { title: 'About', icon: 'mdi-help-box' }
-      ],
-      right: null
+
     }
   }
 }
