2cff0b328373675d95046052638f6c4a483ca324
Edmund Yao <eyao5@my.bcit.ca>
2019-12-02 11:17:57 -0800

    data

---
+++ src/components/recieve.js
@@ -0,0 +1,38 @@
+export default {
+  "+15555555": [
+    {
+      sms_id: "8d7b47fc-be08-4820-8ce3-58fd4fe42a76",
+      direction: "inbound",
+      time: 1522097596060,
+      //   from: "+15555555",
+      //   to: "+16666666",
+      body: "Hello "
+    },
+    {
+      sms_id: "8d7b47fc-be08-4820-8ce3-58fd4fe42a76",
+      direction: "",
+      time: 1522097596060,
+      //   from: "+15555555",
+      //   to: "+16666666",
+      body: " world"
+    },
+    {
+      sms_id: "8d7b47fc-be08-4820-8ce3-58fd4fe42a76",
+      direction: "inbound",
+      time: 1522097596060,
+      //   from: "+15555555",
+      //   to: "+16666666",
+      body: "HI"
+    }
+  ],
+  "+44444444": [
+    {
+      sms_id: "8d7b47fc-be08-4820-8ce3-58fd4fe42a76",
+      direction: "inbound",
+      time: 1522097596060,
+      //   from: "+15555555",
+      //   to: "+16666666",
+      body: "dooooo"
+    }
+  ]
+};
