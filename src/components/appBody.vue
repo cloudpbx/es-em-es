75ee3e11936cbaa3910b5e974e4df9897e6fab86
Jaonl <jason.lu@cloudpbx.ca>
2019-12-02 16:07:43 -0800

    changed name of socket event to 'receiveMessage'

--- src/components/appBody.vue
+++ src/components/appBody.vue
@@ -24,7 +24,7 @@ export default {
     connect: function () {
         console.log('socket connected')
     },
-    newMessage: function (data) {
+    receiveMessage: function (data) {
         console.log('New message recieved' + data.hello)
     }
   },
