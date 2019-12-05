61ecb7085af7d51963917277091d846975403c04
Mohamed Laradji <malaradji@gmail.com>
2019-12-05 10:10:32 -0800

    style(table.sql)

--- expressServer/table.sql
+++ expressServer/table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE polaris_sms(
-id UUID PRIMARY KEY,
-body TEXT,
-from_number TEXT NOT NULL,
-to_number TEXT NOT NULL,
-data JSONB,
-created_at TIMESTAMPTZ default NOW() NOT NULL,
-updated_at TIMESTAMPTZ default NOW() not null,
-deleted_at TIMESTAMPTZ
+    id UUID PRIMARY KEY,
+    body TEXT,
+    from_number TEXT NOT NULL,
+    to_number TEXT NOT NULL,
+    data JSONB,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    deleted_at TIMESTAMPTZ
 );
